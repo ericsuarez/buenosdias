83757ef71a01be13591f166cba4ab1780f4bd4fa
alu0100786330 <alu0100786330@ull.edu.es>
2018-03-22 20:42:09 +0000

    noob

---
+++ bot.js
@@ -0,0 +1,42 @@
+const Telegraf = require('telegraf');
+const bot = new Telegraf("527489649:AAHQKLYJRAvNgTUUKqpUsu6vkW3j4yOhC4o");
+
+const chatid = [];
+
+
+var CronJob = require('cron').CronJob;
+new CronJob('00 36 21 * * 1-5', function() {
+  for(var i =0;i<chatid.length;i++){
+    bot.telegram.sendMessage(chatid[i],"eres un jputa");
+  }
+  
+
+  
+  
+}, null, true, 'Europe/Madrid');
+
+
+
+
+//  bot.telegram.sendMessage("File content at: " + new Date() + " is: \n" + file);
+
+bot.catch((err) => {
+  console.log('Ooops', err)
+})
+
+
+bot.start((ctx) => {
+  console.log('started:', ctx.from.id)
+  chatid.push(ctx.from.id);
+  return ctx.reply('Aitor es usted un hdp! saludos kawais');
+});
+
+
+
+
+
+bot.command('help', (ctx) => ctx.reply('fck you!'));
+
+
+
+bot.startPolling() 
