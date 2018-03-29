321a182c998a4ad72a0d2d16892cd7285e0b6e60
alu0100786330 <alu0100786330@ull.edu.es>
2018-03-29 11:58:44 +0000

    changes pro

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
 const Telegraf = require('telegraf');
 const bot = new Telegraf("527489649:AAHQKLYJRAvNgTUUKqpUsu6vkW3j4yOhC4o");
+const firstname = ["jputillaaa","crackkk del basket","mariposa de jardin", "ornitorrinco mareado","flumifero peollo","saltamontes tartamudo","sangre sucia","renacuajo vomitivo"];
+const secondname = ["hijo del señor","misericordioso perro","hijo de satan","lombriz asustada","meaculos","robot asesino","fenomeno desmesurado","rata rasurada","avestruz lonjeba"];
+const action = ["mate un leopardo","viole un oso","folle un perro","den por el orto","quemen la punta del pene","saquen los ojos","muerdan el clitoris","escupan en la cara","laven con lejia"];
+
+
 
 const chatid = [];
 
 
 var CronJob = require('cron').CronJob;  
 new CronJob('00 23 22 * * 1-5', function() {
+  
+  var rand1 = firstname[Math.floor(Math.random() * firstname.length)];
+  var rand2 = secondname[Math.floor(Math.random() * secondname.length)];
+  
   for(var i =0;i<chatid.length;i++){
-    bot.telegram.sendMessage(chatid[i],"Aitor eres un gran jputa!! Buenas noches cabronazo");
+    bot.telegram.sendMessage(chatid[i],"Aitor eres un " + rand1  + " Buenas noches " + rand2);
   }
 }, null, true, 'Europe/Madrid');
 
@@ -15,8 +24,12 @@ new CronJob('00 23 22 * * 1-5', function() {
 
 var CronJob = require('cron').CronJob;  
 new CronJob('00 00 05 * * 1-5', function() {
+  
+  var rand1 = firstname[Math.floor(Math.random() * firstname.length)];
+  var rand2 = action[Math.floor(Math.random() * action.length)];
+  
   for(var i =0;i<chatid.length;i++){
-    bot.telegram.sendMessage(chatid[i],"Buenos dias jputilla! que te folle un oso");
+    bot.telegram.sendMessage(chatid[i],"Buenos dias " + rand1 + "!! que te " + rand2);
   } 
 }, null, true, 'Europe/Madrid');
 
